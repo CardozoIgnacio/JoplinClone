d5a7f399ce09ba5c131fb2b77d86cf5a972496c9
IgnacioC <cardozojoseignacio@gmail.com>
2020-08-02 16:16:19 -0300

    add dotEnv config var

--- src/controller/controllerIndex.js
+++ src/controller/controllerIndex.js
@@ -1,6 +1,7 @@
 const controllerIndex = {}
 
 function indexRender(req,res){
+   console.log(process.env.DATABASE_URL) 
     res.render('index',{title:"Box Note",version:"1.0",msj:{err:false}})
     
 }
--- src/db/dbConector.js
+++ src/db/dbConector.js
@@ -1,11 +1,14 @@
 const{ Sequelize} = require('sequelize');
 
 // Option 1: Passing parameters separately
+/*
 const db= new Sequelize('loginsequelize', 'admindb', '123..456', {
   host: 'localhost',
   dialect: 'postgres',
   logging: false
 });
-
+console.log(process.env.DATABASE_URL)
+*/
+const db = new Sequelize(process.env.DATABASE_URL,{logging:false})
 module.exports = db
 
