4f54a3db494454d903610877634866a61041bf27
ignacio <cardozojoseignacio@gmail.com>
2020-08-08 11:14:17 -0300

    handelers user Validation

--- src/passport-config.js
+++ src/passport-config.js
@@ -1,21 +1,30 @@
 const localStrategy = require("passport-local").Strategy;
-const Usuario= require('./model/userModel')
-const bcrypt =require('bcrypt')
+const Usuario = require("./model/userModel");
+const bcrypt = require("bcrypt");
 
 function initalize(passport) {
-	const autenticateUser = (user,pass,done) => {
-	
-		Usuario.validUser(user,pass).then().catch()
-	done(null,false,{message:"No existe un usuario con ese nombre"})
-	passport.use(
-		new localStrategy(
-			{ usernameField: "user", passwordField: "pass" },
-			autenticateUser
-		)
-    );
+  const autenticateUser = (user, pass, done) => {
+    Usuario.validUser(user, pass)
+      .then((isValid) => {
+        if (isValid) {
+          done(null, user);
+        } else {
+          done(null, false, { message: "Usuario o contraseña no validos" });
+        }
+      })
+      .catch((err) => console.log("Error de autenticacion", err));// TODO: Renderizar pagina de error
+    done(null, false, { message: "No existe un usuario con ese nombre" });
+  };
 
-    passport.serializeUser((user,done)=>{})
-    passport.deserailizaUser((id,done)=>{})
+  passport.use(
+    new localStrategy(
+      { usernameField: "user", passwordField: "pass" },
+      autenticateUser
+    )
+  );
+
+  passport.serializeUser((user, done) => {});
+  passport.deserailizaUser((id, done) => {});
 }
 
-module.exports= initalize
+module.exports = initalize;
